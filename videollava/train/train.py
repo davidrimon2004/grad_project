e45b37b2d5a7aad004676b24dc3736d7fe1cdde3
davidrimon2004 <davidhalim2004@gmail.com>
2026-09-19 23:37:17 +0300

    fix(train): remove duplicate load_in_8bit/4bit kwargs to fix LlavaLlamaForCausalLM init error

--- videollava/train/train.py
+++ videollava/train/train.py
@@ -919,8 +919,6 @@ def train():
         from transformers import BitsAndBytesConfig
         bnb_model_from_pretrained_args.update(dict(
             device_map={"": training_args.device},
-            load_in_4bit=training_args.bits == 4,
-            load_in_8bit=training_args.bits == 8,
             quantization_config=BitsAndBytesConfig(
                 load_in_4bit=training_args.bits == 4,
                 load_in_8bit=training_args.bits == 8,
